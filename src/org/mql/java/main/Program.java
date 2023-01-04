4512cdf63d5b590b5471753be4300964fb44fe06
Mouhcine Slimani <muhcine.slimani@gmail.com>
2023-01-04 06:51:57 +0100

    update commit

--- src/org/mql/java/main/Program.java
+++ src/org/mql/java/main/Program.java
@@ -1,35 +1,46 @@
 package org.mql.java.main;
 
-import org.mql.java.classparser.ClassParser;
-import org.mql.java.reflection.PackageExplorer;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.stream.Stream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.mql.java.reflection.MyCustomClass;
 import org.mql.java.reflection.ProjectExplorer;
 
 public class Program {
-
-	public Program() {
-		ex01() ; 
+	 
+	public Program() { 
+		ex01(); 
 	}
 
 	private void ex01() {
-		 PackageExplorer pack = new PackageExplorer() ;
-		 ProjectExplorer project = new ProjectExplorer() ; 
-		 ClassParser parser ;
-		 String[] packag = project.getPackages() ; 
-		 for (String p : packag) {
-			String[] classes = pack.getClassList(p) ; 
-			System.out.println(p + " : ");
-			for (String item : classes) {
-				parser = new ClassParser(item);
-				if(parser.isAnnotation())
-					System.out.println("\t\t"+parser.getMyClass());
-			}
-		 }
+		/*
+		PackageExplorer pack = new PackageExplorer();
+		String list[] = pack.getClassList("org.mql.java.reflection");
+		for (String item : list) {
+			System.out.println(item);
+		}
+		*/
+		ProjectExplorer project = new ProjectExplorer("Slimani-mouhcine-UML-Diagrams-Generator") ;
+		project.getProjectPackages();
+		Vector<MyCustomClass> v = project.getMaps();
+		for (MyCustomClass myCustomClass : v) {
+			System.out.println(myCustomClass);
+		}
 	}
-	
-	
-	public static void main(String[] args) {
-		new Program(); 
+ 	public static void main(String[] args) {
+		new Program();
 	}
-	
-
+ 	 
 }
--- src/org/mql/java/reflection/ProjectExplorer.java
+++ src/org/mql/java/reflection/ProjectExplorer.java
@@ -1,22 +1,121 @@
 package org.mql.java.reflection;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 public class ProjectExplorer {
 
-	public ProjectExplorer() {}
-	
-	public String[] getPackages()
-	{
-		Vector<String> pack = new Vector<String>() ;  
-		Package[] pa = Package.getPackages();
-		for (int i = 0; i < pa.length; i++) {
-		    Package p = pa[i]; 
-		    if(p.getName().startsWith("org.mql"))
-		    	pack.add(p.getName());
+	private String projectName;
+	private String path;
+	private String[] pathPart;
+	private String fullProjectPath;
+	private String projectPath;
+	private List<String> packages;
+	private Map<String, String> maps = new HashMap<String, String>();
+
+	public ProjectExplorer(String projectName) {
+		this.projectName = projectName;
+		packages = new Vector<String>();
+		path = System.getProperty("java.class.path");
+		pathPart = path.split("\\\\");
+		projectPath = pathPart[0] + "/" + pathPart[1];
+		if (isProjectExist())
+			this.fullProjectPath = projectPath + "/" + projectName + "/bin";
+	}
+
+	public boolean isProjectExist() {
+		boolean isExist = false;
+		// get the project location
+		File dir = new File(projectPath);
+		File f[] = dir.listFiles();
+		for (File file : f) {
+			// check if the project name exists or no
+			if (file.isDirectory() && file.getName().equals(projectName)) {
+				isExist = true;
+				break;
+			}
 		}
-		String t[] = new String[pack.size()] ; 
-		pack.toArray(t) ; 
-		return t ;
-	}   
+		return isExist;
+	}
+
+	public List<String> getProjectPackages() {
+		File maindir = new File(fullProjectPath);
+		if (maindir.exists() && maindir.isDirectory()) {
+			File arr[] = maindir.listFiles();
+			RecursivePrint(arr, 0, 0);
+		}
+		return new Vector<String>();
+	}
+
+	String line = "";
+	int oldIndex = 0; 
+	Vector<MyCustomClass> v = new Vector<MyCustomClass>();
+	void RecursivePrint(File[] arr, int index, int level) { 
+		if (index == arr.length)
+			return;
+		// System.out.println("----------"+arr[index].getName());
+		// for sub-directories
+		oldIndex = index ; 
+		v.add(new MyCustomClass(index,level,arr[index].getName()));
+		
+		if (arr[index].isDirectory()) {
+			System.out.println(arr[index].getName());
+			// s.append("."+arr[index].getName());
+			// recursion for sub-directories 
+			RecursivePrint(arr[index].listFiles(), 0 , level + 1);
+			// v.add(s.toString());
+			// s = new
+			// StringBuffer(s.substring(0,s.length()-arr[index].getName().length()-1)) ;
+			// System.out.println("------------after :
+			// "+(s.length()-arr[index].getName().length()));
+		}
+		
+		// packages.add(line);
+		// System.out.println("M : "+v.toString());
+		RecursivePrint(arr, ++index, level);
+		//oldIndex=0;
+		// System.out.println("-------------------------");
+		// System.out.println(line);
+		// recursion for main directory
+	}
+
+	public List<String> getPackages() {
+		return packages;
+	}
+
+	public Vector<String> getPack(String name) {
+		Vector<String> pack = new Vector<String>();
+		File dir = new File(fullProjectPath + "/" + name);
+		File f[] = dir.listFiles();
+		for (int i = 0; i < f.length; i++) {
+			if (f[i].isDirectory())
+				pack.add(f[i].getName());
+		}
+		return pack;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public String getFullProjectPath() {
+		return fullProjectPath;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Vector<MyCustomClass> getMaps() {
+		return v;
+	}
+
+	
 }
